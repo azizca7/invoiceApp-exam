6ffe1a9e3b35574deb18db7032cca4afa96194d9
azizca <ikromovazizbek256@gmail.com>
2025-05-31 23:38:53 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen">
       {loading ? <Loader /> : <RouterProvider router={router} />}
     </div>
   );
